b0b3d2b0bc2fa5c4fd2e10bcde7b9e0e6a01c8e5
Ronald Skinner <rjskinnerc@unal.edu.co>
2022-04-04 16:04:33 -0500

    Add click listener to the menu button
    Update the CSS styles in order to reuse the menu in mobile and desktop

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const menuBtn = document.querySelector('#menu-btn');
-const headerNavMobile = document.querySelector('#menu-nav');
+const menuNav = document.querySelector('#menu-nav');
 const navLinks = document.querySelectorAll('#headline #about-me #contact');
+
+menuBtn.addEventListener('click', () => {
+  menuNav.classList.toggle("menu-hidden")
+})
