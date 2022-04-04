654fac8d594f4be76f2205201100dbed6ef058b0
Ronald Skinner <rjskinnerc@unal.edu.co>
2022-04-04 16:17:05 -0500

    Add the close menu button and its listener

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const menuBtn = document.querySelector('#menu-btn');
 const menuNav = document.querySelector('#menu-nav');
+const closeMenuBtn = document.querySelector('#close-menu')
 const navLinks = document.querySelectorAll('#headline #about-me #contact');
 
 menuBtn.addEventListener('click', () => {
+  menuNav.classList.toggle("menu-hidden")
+})
+
+closeMenuBtn.addEventListener("click", () => {
   menuNav.classList.toggle("menu-hidden")
 })
