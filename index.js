c57bd1bbb3b5691a16a8a1abb665dd6d88ed206d
ameerhamzakhalid <ameerhamza.khalid1997@gmail.com>
2022-04-05 01:26:53 +0500

    Update index.js
    Added reference links of menu

---
+++ index.js
@@ -0,0 +1,3 @@
+const menuBtn = document.querySelector('#menu-btn');
+const headerNavMobile = document.querySelector('#menu-nav');
+const navLinks = document.querySelectorAll('#headline #about-me #contact');
