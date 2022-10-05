a42d733fdc58601b7daae190007ca55fda279b12
Ronald Skinner <rjskinnerc@unal.edu.co>
2022-10-05 15:54:27 -0500

    Config Jest and add index page tests

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,14 @@
-const nextJest = require('next/jest')
+const nextJest = require("next/jest");
 
 const createJestConfig = nextJest({
-  dir: './',
-})
+  dir: "./",
+});
 
 /** @type {import('jest').Config} */
 const customJestConfig = {
-  moduleDirectories: ['node_modules', '<rootDir>/'],
-  testEnvironment: 'jest-environment-jsdom',
-}
+  moduleDirectories: ["node_modules", "<rootDir>/"],
+  testEnvironment: "jest-environment-jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+};
 
-module.exports = createJestConfig(customJestConfig)
+module.exports = createJestConfig(customJestConfig);
