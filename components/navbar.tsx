81d6fcc92e967bf6eefa185d497887ab7f80bcac
Ronald Skinner <rjskinnerc@unal.edu.co>
2022-10-05 23:04:58 -0500

    Add menu visibility and toggler

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,5 +1,6 @@
 import classNames from "classnames";
-import { enterScope } from "immer/dist/internal";
+import { TbLayoutSidebarLeftCollapse } from "react-icons/tb";
+import { IoMenu } from "react-icons/io5";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 
@@ -11,15 +12,16 @@ const Navbar = () => {
     { name: "PROJECTS", id: "#projects" },
     { name: "CONTACT", id: "#contact" },
   ]);
-  const [active, setActive] = useState('#heading');
-  
+  const [active, setActive] = useState("#heading");
+  const [isOpen, setIsOpen] = useState(false);
+
   useEffect(() => {
     let observer = new IntersectionObserver(handleSectionChange, {
       root: null,
       rootMargin: "0px",
       threshold: 0.5,
     });
-    sections.forEach((section: {id: string, name: string}) => {
+    sections.forEach((section: { id: string; name: string }) => {
       observer.observe(document.querySelector(section.id)!);
     });
   }, [sections]);
@@ -27,33 +29,60 @@ const Navbar = () => {
   const handleSectionChange = (entries: IntersectionObserverEntry[]) => {
     entries.forEach((entry: IntersectionObserverEntry) => {
       if (entry.isIntersecting) {
-        setActive(`#${entry.target.id}`);        
+        setActive(`#${entry.target.id}`);
       }
     });
   };
 
   const isActive = (tag: string): string => {
     return classNames({
-      'text-sky-600': tag === active,
-    })
-  }
+      "text-sky-600": tag === active,
+    });
+  };
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const showMenu = classNames({
+    hidden: !isOpen,
+  });
 
   return (
-    <nav className="h-screen bg-white absolute z-10 w-full flex flex-col items-center gap-6">
-      <Image
-        src="/vercel.svg"
-        width={100}
-        height={100}
-        alt="Ronald Skinner Dev"
+    <>
+      <nav
+        className={
+          "h-screen bg-white fixed z-50 w-full flex flex-col items-center gap-10 " +
+          showMenu
+        }
+      >
+        <Image
+          src="/vercel.svg"
+          width={100}
+          height={100}
+          alt="Ronald Skinner Dev"
+        />
+        <ul className="flex flex-col w-full items-center gap-4 font-semibold font-exo">
+          {sections.map((section) => (
+            <li
+              key={section.name}
+              className={isActive(section.id)}
+              onClick={() => setIsOpen(false)}
+            >
+              <a href={section.id}>{section.name}</a>
+            </li>
+          ))}
+        </ul>
+        <TbLayoutSidebarLeftCollapse
+          className="absolute right-0 bottom-20 text-4xl text-sky-600 drop-shadow cursor-pointer"
+          onClick={() => setIsOpen(false)}
+        />
+      </nav>
+      <IoMenu
+        className="fixed top-3 right-3 text-4xl text-sky-600 z-10 drop-shadow cursor-pointer"
+        onClick={() => toggleMenu()}
       />
-      <ul className="flex flex-col w-full items-center gap-3 font-semibold font-exo">
-        {sections.map((section) => (
-          <li key={section.name} className={isActive(section.id)}>
-            <a href={section.id}>{section.name}</a>
-          </li>
-        ))}
-      </ul>
-    </nav>
+    </>
   );
 };
 
