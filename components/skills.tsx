1c7b7ad845fa475f69af4afdd57390dd1146a712
Ronald Skinner <rjskinnerc@unal.edu.co>
2022-10-07 14:50:04 -0500

    Add tite and basic stiles to the skills section
    - Refactored about me paragraph

--- components/skills.tsx
+++ components/skills.tsx
@@ -1,7 +1,15 @@
 const Skills = () => {
   return (
-    <section id="skills" className="h-screen">
-      <h1>My Skills</h1>
+    <section id="skills" className="pt-20">
+      <div className="relative h-20 md:mt-6 lg:mt-16 md:w-11/12 xl:w-10/12">
+        <h1 className="font-exo text-3xl font-bold absolute top-0.5 right-0 pr-3 tracking-wide z-10 text-sky-600 2xl:right-20">
+          My Skills
+        </h1>
+        <h2 className="font-exo text-5xl font-bold absolute top-0 right-0 pr-3 tracking-wide text-fuchsia-600/10 z-0 2xl:right-20">
+          My Skills
+        </h2>
+      </div>
+      <div className="my-10 flex flex-col gap-10 md:flex-row md:w-10/12 xl:w-8/12 md:mx-auto"></div>
     </section>
   );
 };
