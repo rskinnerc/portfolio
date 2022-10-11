91570c090dd97841497a453f21c84f88828a944d
Ronald Skinner <rjskinnerc@unal.edu.co>
2022-10-11 15:40:46 -0500

    Add basic contact section

--- components/contact.tsx
+++ components/contact.tsx
@@ -1,7 +1,14 @@
 const Contact = () => {
   return (
-    <section id="contact" className="h-screen">
-      <h1>Contact Me</h1>
+    <section id="contact" className="py-20 bg-gradient-to-b from-white to-sky-100">
+      <div className="relative h-20 md:mt-6 lg:mt-16 md:w-11/12 xl:w-10/12">
+        <h1 className="font-exo text-3xl font-bold absolute top-0.5 right-0 pr-3 tracking-wide z-10 text-sky-600 2xl:right-20">
+          Contact Me
+        </h1>
+        <h2 className="font-exo text-5xl font-bold absolute top-0 right-0 pr-3 tracking-wide text-fuchsia-600/10 z-0 2xl:right-20">
+          Contact Me
+        </h2>
+      </div>
     </section>
   );
 };
